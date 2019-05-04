94d4945ef7be27a719f92207614b35fbc5dcced6
Alan Foster <alan@alanfoster.me>
2019-05-05 00:48:26 +0100

    Add initial compiler entrypoint for cli

---
+++ compiler.py
@@ -0,0 +1,46 @@
+import antlr4
+from pathlib import Path
+from dataclasses import dataclass
+from compiler import compiler
+from typing import List, Any
+from wasm.printer import pretty_print
+import sys
+
+
+@dataclass(frozen=True)
+class CompileTarget:
+    file: Path
+
+    @property
+    def output(self):
+        return self.file.with_suffix(".wast")
+
+
+@dataclass(frozen=True)
+class Configuration:
+    targets: List[CompileTarget]
+
+
+def parse_argv(argv: List[Any]) -> Configuration:
+    input_path = Path(argv[1]).absolute()
+    files = [input_path] if input_path.is_file() else list(input_path.glob("*.bug"))
+    targets = []
+    for path in files:
+        targets.append(CompileTarget(file=path))
+
+    return Configuration(targets=targets)
+
+
+def main(argv: List[Any]) -> None:
+    configuration = parse_argv(argv)
+
+    for target in configuration.targets:
+        input_stream = antlr4.FileStream(str(target.file))
+        result = compiler.generate(input_stream)
+
+        with target.output.open(mode="w") as file:
+            file.write(pretty_print(result))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
